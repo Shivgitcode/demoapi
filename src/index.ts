82f0723d22dd246efa8e0fac326f60a42c8a6e6f
Shivgitcode <113466325+Shivgitcode@users.noreply.github.com>
2025-02-11 15:26:51 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,10 @@ const app = express();
 
 app.use("/api/v1/", userRouter);
 
+app.get("/",(req,res)=>{
+    res.send("hello")
+}
+
 app.listen(port, () => {
     console.log("listening on port 8000")
 })
